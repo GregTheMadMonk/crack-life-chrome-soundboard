ae2ec9e7536321f5ef404a5b485f2799f99c05e3
GregTheMadMonk <yagreg7@gmail.com>
2017-12-05 16:37:08 +0300

    Volume settings added

--- js/events.js
+++ js/events.js
@@ -5,30 +5,73 @@ audio.play();*/
 var daud;
 daud = new Audio("../sound/dload.wav");
 
+var volume = 1.0;
+var tvolume;
+
 function sleep(ms) {
 ms += new Date().getTime();
 while (new Date() < ms){}
 } 
 
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+	if (request.type == 'vol') 
+	{
+		if ((volume != 0) || (request.name != 0)) tvolume = volume;
+		if (request.name > 1) volume = 1;
+		else if (request.name < 0) volume = 0;
+			else volume = request.name;
+	}
+	if (request.type == 'mute')
+	{
+		if (volume == 0.0) volume = tvolume;
+		else {
+			tvolume = volume;
+			volume = 0.0;	
+		}
+	}
+	if (request.type == 'get_mute')
+	{
+		var n = 'f';
+		if (volume == 0.0) n = 't';
+		chrome.runtime.sendMessage({ type: 'got_mute', name: n } );
+	}
+	if (request.type == 'get_volume')
+	{
+		chrome.runtime.sendMessage({ type: 'got_volume', name: volume } );
+	}
+});
+
 chrome.tabs.onActiveChanged.addListener(
 	function (tabId, selectInfo) {
-		(new Audio("../sound/no.wav")).play();
+		var audio  = new Audio("../sound/no.wav");
+		audio.volume = volume;
+		audio.play();
 	}
 );
+
+chrome.tabs.onRemoved.addListener(function(tabid, removed) {
+	var audio = new Audio("../sound/muted.wav");
+	audio.volume = volume;
+	audio.play();
+})
 
 chrome.tabs.onUpdated.addListener(
 function ( tabId, changeInfo, tab )
 {
 	if ( changeInfo.status === "loading" )
     {
-		(new Audio("../sound/refresh.wav")).play();
+		var audio = new Audio("../sound/refresh.wav");
+		audio.volume = volume;
+		audio.play();
     }
 	
 	
 	if ( changeInfo.status === "complete" )
     {
 		sleep(250);
-		(new Audio("../sound/loaded.wav")).play();
+		var audio = new Audio("../sound/loaded.wav")
+		audio.volume = volume;
+		audio.play();
     }
 }
 );
@@ -36,6 +79,7 @@ function ( tabId, changeInfo, tab )
 chrome.downloads.onChanged.addListener(
 	function (downloadItem) {
 		daud.currentTime = 0;
+		daud.volume = volume;
 
 		if (downloadItem.state == undefined)
 		{
@@ -46,7 +90,9 @@ chrome.downloads.onChanged.addListener(
 		{		
 			daud.pause();
 		
-			(new Audio("../sound/cancelled.wav")).play();
+			var audio = new Audio("../sound/cancelled.wav");
+			audio.volume = volume;
+			audio.play();
 		}
 	}
 );
--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,53 @@
-(new Audio("../sound/doyou.wav")).play();
+chrome.runtime.sendMessage({ type: 'get_mute' });
+chrome.runtime.sendMessage({ type: 'get_volume' });
+
+var priv = new Audio("../sound/doyou.wav");
+priv.play();
 
 function openInNewTab(url) {
   var win = window.open(url, '_blank');
   win.focus();
 }
 
-function l1() {
+function l1() 
+{
 	openInNewTab("mailto:yagreg7@gmail.com");
 };
 
-function l2() {
+function l2() 
+{
 	openInNewTab("https://github.com/GregTheMadMonk");
 };
 
+function mute()
+{
+	chrome.runtime.sendMessage({ type: 'mute' } );
+	chrome.runtime.sendMessage({ type: 'get_volume' });
+};
+
+function volChanged()
+{
+	chrome.runtime.sendMessage({ type: 'vol', name: document.getElementById("volume").value });
+	chrome.runtime.sendMessage({ type: 'get_mute' });
+	chrome.runtime.sendMessage({ type: 'get_volume' });
+};
+
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+	if (request.type == 'got_mute')
+	{
+		if (request.name == 't') document.getElementById("mute").checked = true;
+		else document.getElementById("mute").checked = false;
+	}
+	if (request.type = 'got_volume')
+	{
+		document.getElementById("volume").value = request.name;
+		priv.volume = document.getElementById("volume").value;
+	}
+});
+
 document.getElementById("l1").onclick = l1;
 document.getElementById("l2").onclick = l2;
+document.getElementById("mute").onclick = mute;
+document.getElementById("volume").onchange = volChanged;
+
+
