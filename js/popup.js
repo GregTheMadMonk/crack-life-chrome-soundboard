2c1b00808cac64a37ad7205506b5fabccf655289
GregTheMadMonk <yagreg7@gmail.com>
2018-11-08 17:13:45 +0300

    Minor popup changes

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,9 @@ chrome.runtime.sendMessage({ type: 'get_volume' });
 var priv = new Audio("../sound/doyou.wav");
 priv.play();
 
+var no;
+no = new Audio("../sound/no.wav");
+
 function openInNewTab(url) {
   var win = window.open(url, '_blank');
   win.focus();
@@ -25,8 +28,18 @@ function mute()
 	chrome.runtime.sendMessage({ type: 'get_volume' });
 };
 
+function volSliderChanged()
+{
+	no.currentTime = 0;
+	no.play();
+	document.getElementById("volume").value = document.getElementById("volume_slider").value / 100;
+	volChanged();
+};
+
 function volChanged()
 {
+	no.currentTime = 0;
+	no.play();
 	chrome.runtime.sendMessage({ type: 'vol', name: document.getElementById("volume").value });
 	chrome.runtime.sendMessage({ type: 'get_mute' });
 	chrome.runtime.sendMessage({ type: 'get_volume' });
@@ -41,13 +54,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback) {
 	if (request.type = 'got_volume')
 	{
 		document.getElementById("volume").value = request.name;
+		document.getElementById("volume_slider").value = request.name * 100;
 		priv.volume = document.getElementById("volume").value;
+		no.volume = document.getElementById("volume").value;
 	}
 });
 
 document.getElementById("l1").onclick = l1;
 document.getElementById("l2").onclick = l2;
 document.getElementById("mute").onclick = mute;
+document.getElementById("volume_slider").onchange = volSliderChanged;
+document.getElementById("volume_slider").oninput = volSliderChanged;
 document.getElementById("volume").onchange = volChanged;
 
 
