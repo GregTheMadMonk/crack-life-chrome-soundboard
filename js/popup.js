6215dbdd7208318c602f6b66e659ec6cb50983bf
GregTheMadMonk <yagreg7@gmail.com>
2018-11-12 22:32:44 +0300

    Half-Life menu on-hover sound added to popup

--- js/popup.js
+++ js/popup.js
@@ -10,7 +10,15 @@ no = new Audio("../sound/no.wav");
 function openInNewTab(url) {
   var win = window.open(url, '_blank');
   win.focus();
-}
+};
+
+function menuItemHollow()
+{
+	var hollow_sound = new Audio("../sound/buttonrollover.wav");
+	hollow_sound.volume = Math.sqrt(document.getElementById("volume").value); // sound efect is not loud enough 
+										 // we need a higher volume from 0 to 1 -> sqrt(x) >= x when 0<=x<=1
+	hollow_sound.play();
+};
 
 function l1() 
 {
@@ -60,11 +68,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback) {
 	}
 });
 
+
+document.getElementById("cl_icon").onclick = menuItemHollow;
+document.getElementById("lmao_me").onclick = menuItemHollow;
+
 document.getElementById("l1").onclick = l1;
+document.getElementById("l1").onmouseover = menuItemHollow;
 document.getElementById("l2").onclick = l2;
+document.getElementById("l2").onmouseover = menuItemHollow;
 document.getElementById("mute").onclick = mute;
+document.getElementById("mute").onmouseover = menuItemHollow;
 document.getElementById("volume_slider").onchange = volSliderChanged;
 document.getElementById("volume_slider").oninput = volSliderChanged;
+document.getElementById("volume_slider").onmouseover = menuItemHollow;
 document.getElementById("volume").onchange = volChanged;
 
 
